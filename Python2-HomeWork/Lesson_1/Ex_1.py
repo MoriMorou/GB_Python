296638a768e596f9ca0cd3029009c37b3f776194
Mori Morou <mori_morou@yahoo.com>
2018-12-18 17:33:20 +0300

    Added new task

--- Python2-HomeWork/Lesson_1/Ex_1.py
+++ Python2-HomeWork/Lesson_1/Ex_1.py
@@ -1,19 +1,23 @@
-# Начало
-# Вывод("Введите два числа")
-# Ввод(Первое число)
-# Ввод(Второе число)
-# Если второечисло != 0
-# то частное = первое число / второе число
-# Вывод("Результат равен", частное)
-# иначе Вывол ("нет решения")
-# Конец
+# 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 
-print('Input two numbers')
-a = int(input('Input first number'))
-b = int(input('Input second number'))
+print('Find the sum and product of the digits of a three-digit number.')
+num = int(input('Input a three-digit number'))
+if num > 0:
+    n1 = int(num / 100)
+    n2 = int(num / 10 % 10)
+    n3 = int(num % 10)
+    print(n1, n2, n3, '\n')
 
-if b != 0:
-    c = a/ b
-    print(f'Result is {c}')
+    a = n1 + n2 + n3
+    b = n1 * n2 * n3
+    print(a, b, '\n')
+
 else:
-    print('No solution')
+    n1 = int(num / 100)
+    n2 = int(num * -1 / 10 % 10)
+    n3 = int(num * -1 % 10)
+    print(n1, n2, n3, '\n')
+
+    a = n1 + n2 + n3
+    b = n1 * n2 * n3
+    print(a, b, '\n')

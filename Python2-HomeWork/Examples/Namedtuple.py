eaaa7f9c5ed78dcb44adb2db4170cfaaf9dda856
Mori Morou <mori_morou@yahoo.com>
2019-01-25 13:11:21 +0300

    added

--- Python2-HomeWork/Examples/Namedtuple.py
+++ Python2-HomeWork/Examples/Namedtuple.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 
+# Именновонный кортеж
+
 # hero_1 = ('Vasya', 'man', 180, 84, 46, 46)
 # print(hero_1[4])
 #
@@ -18,13 +20,15 @@ print(hero_2)
 
 print('*' * 50)
 Point = namedtuple('Point', ['x', 'y', 'z'])
-# Point = namedtuple('Point', ['x', 'y', 'z'], defaults=[0, 0, 0])
+# Point = namedtuple('Point', ['x', 'y', 'z'], defaults=[0, 0, 0])  - нужно разобрать
 p1 = Point(2, z=4, y=6)
-print(p1)
+p2 = Point(66, 23, 23)
+print(p1, p2)
 
 print(p1.x)
 # p1.x = 42
 p1 = p1._replace(x=42)
-print(p1)
+p3 = p1._replace(z=13)
+print(p1, p2, p2, sep='\n')
 
 print(p1._fields)

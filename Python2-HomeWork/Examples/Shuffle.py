94794c0b2e06331940e02ae3a9aee46b51a7b016
Mori <mori_morou@rocketmail.com>
2019-07-11 10:56:15 +0300

    deleted

--- Python2-HomeWork/Examples/Shuffle.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# Перемешивание массива
-
-array = [i for i in range(10)]
-print("Old array", array)
-random.shuffle(array)
-print("New array", array)
--- Python2-HomeWork/Lesson_7/HomeWork7/test.py
+++
@@ -1,5 +0,0 @@
-def calc(x):
-    y = 2 * abs(x) -1
-    print(y)
-
-calc(-2)

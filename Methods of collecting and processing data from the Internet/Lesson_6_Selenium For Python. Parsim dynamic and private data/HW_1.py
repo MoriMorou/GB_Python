4f0468c2ba8ccec1bc94b83c0fdf83143d5ff377
Mori <mori_morou@rocketmail.com>
2019-08-07 13:40:29 +0300

    hw 1

--- Methods of collecting and processing data from the Internet/Lesson_6_Selenium For Python. Parsim dynamic and private data/HW_1.py
+++ Methods of collecting and processing data from the Internet/Lesson_6_Selenium For Python. Parsim dynamic and private data/HW_1.py
@@ -1,40 +1,55 @@
-from selenium import webdriver          #Основной элемент
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait   # Select - Поле множественного выбора
-from selenium.webdriver.common.by import By  #используем Селениум
-from selenium.webdriver.common.keys import Keys    #Клавиши клавиатуры
-import pprint
+import time
+from selenium import webdriver  # Основной элемент
+from selenium.webdriver.common.keys import Keys  # Клавиши клавиатуры
 from pymongo import MongoClient
 
-link = "https://mail.ru/"
-driver = webdriver.Chrome()
-driver.get(link)
 
-assert "Mail.ru: почта" in driver.title
-driver.find_element_by_id("mailbox:login").send_keys('mori_morou')
-Select(driver.find_element_by_id('mailbox:domain')).select_by_visible_text("@mail.ru")
-driver.find_element_by_id("mailbox:password").send_keys('Volkodav!13')
-driver.find_element_by_id("mailbox:submit").click()
+def mail_site():
+    driver = webdriver.Chrome()
+    driver.get('https://mail.ru')
+    elem = driver.find_element_by_id('mailbox:login')
+    elem.send_keys('Mori_morou@mail.ru')
+    elem = driver.find_element_by_id('mailbox:password')
+    elem.send_keys('Volkodav13')
+    elem.send_keys(Keys.RETURN)
+    time.sleep(8)  # Download page maybe so long
+    letters_class_name = 'llc js-tooltip-direction_letter-bottom js-letter-list-item llc_normal'
+    len_letters = len(driver.find_elements_by_xpath(f'//a[contains(@class, "{letters_class_name}")]'))
+    letters_all = []
+    for i in range(len_letters):
+        letters = driver.find_elements_by_xpath(f'//a[contains(@class, "{letters_class_name}")]')
+        letters[i].click()
+        time.sleep(3)
+        letter_author = driver.find_element_by_class_name('letter__contact-item').text
+        letter_date = driver.find_element_by_class_name('letter__date').text
+        letter_topic = driver.find_element_by_class_name('thread__subject').text
+        letter_content = driver.find_element_by_class_name('letter-body').text
+        letters_all.append({'author': letter_author,
+                            'date': letter_date,
+                            'topic': letter_topic,
+                            'content': letter_content})
+        driver.get('https://e.mail.ru/inbox/')
+        time.sleep(3)
+    driver.quit()
+    return letters_all
 
-wait = WebDriverWait(driver, 15);
-wait.until(EC.title_contains("Входящие"));
-assert "Входящие" in driver.title
 
-all_items = driver.find_elements_by_xpath("//a[contains(@class,'llc js-tooltip-direction_letter-bottom js-letter-list-item llc_normal')]")
-
-letters_info = []
-for item in all_items:
-#     print(item.get_attribute('innerHTML'))
-    in_letter_link = item.get_attribute('href')
-    one_letter_info = item.find_element_by_class_name("llc__content")
-    author = one_letter_info.find_element_by_class_name("ll-crpt").get_attribute('title')
-    subj = one_letter_info.find_element_by_class_name("ll-sj__normal").text
-    date = item.find_element_by_class_name("llc__item_date").get_attribute('title')
-    print("From: ", author, "\t\t", date, "\nSubj:", subj, "\nLink:", in_letter_link, "\n\n")
-    letters_info.append({"author": author, "date": date, "subj": subj, "link": in_letter_link})
-
-pprint.pprint(letters_info)
+def mongoDB_data():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    data_base = client['db_mail_letters']  # db name
+    mail_letters = data_base.mail_letters  # collection name
+    return mail_letters
 
 
+def save_to_mongoDB(letters: list):
+    mail_letters = mongoDB_data()
+    count = 0
+    for letter in letters:
+        spam = mail_letters.find_one({'content': letter['content']})
+        if spam is None:
+            mail_letters.insert_one(letter)
+            count += 1
+    print(f'Added {count} records. Collection "{mail_letters.name}" has {mail_letters.count_documents({})} letters')
 
 
+save_to_mongoDB(mail_site())
